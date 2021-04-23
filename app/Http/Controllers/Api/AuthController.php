08f7ffa24f647d36ac2582dccab74a0a48f445ef
ripon <riponahmed2201@gmail.com>
2021-04-24 02:15:23 +0600

    user added sucess

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -15,7 +17,12 @@ class AuthController extends Controller
      */
     public function index()
     {
-      return User::all();
+        $users = User::all();
+        return response()->json([
+            "success" => true,
+            "message" => 'Display a listing of the resource.',
+            "data" => $users
+        ]);
     }
 
     /**
@@ -36,7 +43,39 @@ class AuthController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // return $request->all();
+       $validator = Validator::make($request->all(),[
+        'name' => 'required|string',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:6',
+       ]);
+
+       if($validator->fails()){
+            return response()->json([
+                "success" => false,
+                "errors"=>$validator->errors()
+            ],401);
+       }
+
+       try {
+        $user = User::create([
+                'name'=> $request->name,
+                'email'=> $request->email,
+                'password'=> Hash::make($request->password),
+            ]);
+
+            return response()->json([
+                "data" => $user,
+                "success" => true,
+                "errors"=>"User created successfully."
+            ],400);
+
+       } catch (\Exception $e) {
+            return response()->json([
+                "success" => false,
+                "errors"=>"Somthing wrong"
+            ],400);
+       }
     }
 
     /**
@@ -47,7 +86,12 @@ class AuthController extends Controller
      */
     public function show($id)
     {
-        //
+       $user = User::find($id);
+
+        return response()->json([
+            "message" => 'Display the specified resource.',
+            "data" => $user
+        ]);
     }
 
     /**
@@ -81,6 +125,11 @@ class AuthController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // return User::destroy($id);
+        $user =  User::find($id)->delete();
+        return response()->json([
+            "message" => 'Remove the specified resource from storage.',
+            "data" => $user
+        ]);
     }
 }
